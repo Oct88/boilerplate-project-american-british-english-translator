6c0bb2a4504c2d880e5d831e3e627c2d00fe855e
Laviniu <83814772+Oct88@users.noreply.github.com>
2021-11-08 11:37:34 +0200

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -9,4 +9,104 @@ let Translator = require('../components/translator.js');
 
 suite('Functional Tests', () => {
 
+  test('POST /api/translate text and locale', function(done) {
+    let text = 'Something to translate';
+    let locale = 'american-to-british';
+    chai
+      .request(server)
+      .post('/api/translate')
+      .send({text: text, locale: locale})
+      .end((req, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, 'application/json');
+        assert.property(res.body, 'text');
+        assert.property(res.body, 'translation');
+        assert.equal(res.body.text, text);
+        assert.equal(res.body.translation, 'Everything looks good to me!');
+        done();
+      });
+  });
+
+  test('POST /api/translate text and invalid locale', function(done) {
+    let text = 'Something to translate';
+    let locale = undefined;
+    chai
+      .request(server)
+      .post('/api/translate')
+      .send({text: text, locale: locale})
+      .end((req, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, 'application/json');
+        assert.property(res.body, 'error');
+        assert.equal(res.body.error, 'Required field(s) missing');
+        done();
+      });
+  });
+
+  test('POST /api/translate missing text', function(done) {
+    let text = undefined;
+    let locale = 'american-to-british';
+    chai
+      .request(server)
+      .post('/api/translate')
+      .send({text: text, locale: locale})
+      .end((req, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, 'application/json');
+        assert.property(res.body, 'error');
+        assert.equal(res.body.error, 'Required field(s) missing');
+        done();
+      });
+  });
+
+  test('POST /api/translate missing locale', function(done) {
+    let text = 'text';
+    let locale = '';
+    chai
+      .request(server)
+      .post('/api/translate')
+      .send({text: text, locale: locale})
+      .end((req, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, 'application/json');
+        assert.property(res.body, 'error');
+        assert.equal(res.body.error, 'Invalid value for locale field');
+        done();
+      });
+  });
+
+  test('POST /api/translate empty text', function(done) {
+    let text = '';
+    let locale = 'american-to-british';
+    chai
+      .request(server)
+      .post('/api/translate')
+      .send({text: text, locale: locale})
+      .end((req, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, 'application/json');
+        assert.property(res.body, 'error');
+        assert.equal(res.body.error, 'No text to translate');
+        done();
+      });
+  });
+
+  test('POST /api/translate text that needs no translation', function(done) {
+    let text = 'text';
+    let locale = 'american-to-british';
+    chai
+      .request(server)
+      .post('/api/translate')
+      .send({text: text, locale: locale})
+      .end((req, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, 'application/json');
+        assert.property(res.body, 'text');
+        assert.property(res.body, 'translation');
+        assert.equal(res.body.text, text);
+        assert.equal(res.body.translation, 'Everything looks good to me!');
+        done();
+      });
+  });
+
 });
