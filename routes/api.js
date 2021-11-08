e4053aaad88261a134eb21e8e63d3b46212d38b9
Laviniu <83814772+Oct88@users.noreply.github.com>
2021-11-08 11:19:10 +0200

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,36 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+
+      if (req.body.text == undefined || req.body.locale == undefined) {
+        res.json({error: 'Required field(s) missing'});
+
+      } else if (!req.body.text) {
+        res.json({error: 'No text to translate'});
+
+      } else {
+        let text = req.body.text;
+        let locale = req.body.locale;
+
+        if (locale == 'american-to-british') {
+          let result = translator.amerToBrit(text);
+          if (result != text) {
+            res.json({text: text, translation: result});
+          } else {
+            res.json({text: text, translation: 'Everything looks good to me!'});
+          }
+
+        } else if (locale == 'british-to-american') {
+          let result = translator.britToAmer(text);
+          if (result != text) {
+            res.json({text: text, translation: result});
+          } else {
+            res.json({text: text, translation: 'Everything looks good to me!'});
+          }
+          
+        } else if (locale != 'american-to-british' && locale != 'british-to-american') {
+          res.json({error: 'Invalid value for locale field'});
+        }
+      }
     });
 };
